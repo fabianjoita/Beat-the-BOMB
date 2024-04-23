d4ec47b9a0ed36c8ec2247b9d5211efb2b3ad3c3
unknown <joitafabi@gmail.com>
2024-04-23 12:01:13 +0300

    Jumatatea adaugarii intrebarilor din subgrafuri

--- Codul_in_C/incercare.c
+++ Codul_in_C/incercare.c
@@ -16,16 +16,68 @@ struct Graph {
     struct node* head[MAX_VERTICES_GRAPH];
 };
 
+struct node_secund {
+    int dest;
+    char continut_nod_graf_secundar[MAX_LINE_LENGTH];
+    struct node_secund* next;
+};
+
+struct Graph_secund {
+    struct node_secund* head[MAX_VERTICES_GRAPH];
+};
+
 struct Edge {
     int src, dest;
 };
 
-    //impreuna cu creearea grafului o sa citim si continutul pentru fiecare nod categorie
+struct Graph_secund* createGraphSecund(struct Edge edges[], int num_edges, int num_vertices, struct node* node1) {
+    struct Graph_secund* graph_secund = (struct Graph_secund*)malloc(sizeof(struct Graph_secund));
+
+    FILE *fisier_continut_quiz = fopen("quiz_intrebari.txt", "r");
+    if (fisier_continut_quiz == NULL) {
+        fprintf(stderr, "Nu s-a putut deschide fisierul.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 1; i < num_vertices; i++) {
+        graph_secund->head[i] = NULL;
+    }
+
+    struct node_secund* newnode = (struct node_secund*)malloc(sizeof(struct node_secund));
+    newnode->dest = 1;
+    newnode->next = NULL;
+    strcpy(newnode->continut_nod_graf_secundar, "Continutul primului nod secundar.");
+    node1->next = newnode;
+
+    char line[MAX_LINE_LENGTH];
+    int cnt_edge = 1;
+    for (int i = 1; i < num_edges; i++) {
+        int src = cnt_edge;
+        int dest = src + 1;
+
+        newnode = (struct node_secund*)malloc(sizeof(struct node_secund));
+        newnode->dest = dest;
+
+        // Citim intrebarea nodului
+        if (fgets(line, sizeof(line), fisier_continut_quiz) != NULL) {
+            line[strcspn(line, "\n")] = '\0'; // Eliminăm caracterul newline de la sfârșitul liniei
+            strncpy(newnode->continut_nod_graf_secundar, line, sizeof(newnode->continut_nod_graf_secundar));
+        }
+
+        newnode->next = graph_secund->head[src];
+        graph_secund->head[src] = newnode;
+        
+    }
+
+    fclose(fisier_continut_quiz);
+    return graph_secund;
+}
+
 
 struct Graph* createGraph(struct Edge edges[], int num_edges, int num_vertices) {
     struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
 
-    FILE *fisier_continut_quiz = fopen("quiz.txt", "r");
+    FILE *fisier_continut_quiz = fopen("quiz_categorii.txt", "r");
     if (fisier_continut_quiz == NULL) {
         fprintf(stderr, "Nu s-a putut deschide fisierul.\n");
         exit(EXIT_FAILURE);
@@ -47,14 +99,15 @@ struct Graph* createGraph(struct Edge edges[], int num_edges, int num_vertices) 
         if (fgets(line, sizeof(line), fisier_continut_quiz) != NULL) {
             line[strcspn(line, "\n")] = '\0'; // Eliminăm caracterul newline de la sfârșitul liniei
             strncpy(newnode->continut_nod_graf_principal, line, sizeof(newnode->continut_nod_graf_principal));
-        }   
-
-            // Trecem peste liniile de întrebări până la întâlnirea unui rând gol
-        while (fgets(line, sizeof(line), fisier_continut_quiz) != NULL && strcmp(line, "\n") != 0) {
         }
 
         newnode->next = graph->head[src];
         graph->head[src] = newnode;
+        
+        // O sa facem graful secund pt fiecare nod pricipal
+        // fiecare nod principal o sa contina 4 intrebari (alte 4 noduri formand un subgraf)   
+        struct Edge edges_secund[4]; // muchii pentru graf secundar     
+        struct Graph_secund* graph_secund = createGraphSecund(edges, 4, 5, newnode);
     }
 
     fclose(fisier_continut_quiz);
@@ -101,11 +154,12 @@ int main(void) {
 
     num_vertices++;
 
-    struct Edge edges[num_edges];
+    struct Edge edges[num_edges]; // pentru graf principal
     int index = 0;
 
     rewind(fisier);
-
+    
+    // pentru graful principal
     while (fgets(line, MAX_LINE_LENGTH, fisier) != NULL) {
         token = strtok(line, ",");
         src = atoi(token);
